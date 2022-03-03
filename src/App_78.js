2fb43748c6bff2916aa1fcdb63dd6c6019b5999b
209410678 <209410678@gms.tku.edu.tw>
2022-03-03 22:14:17 +0800

    first commit

---
+++ src/App_78.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar_78 from './Navbar_78'
+import Hero_78 from './Hero_78'
+import Sidebar_78 from './Sidebar_78'
+import Submenu_78 from './Submenu_78'
+function App_78() {
+  return (
+    <>
+      <Navbar_78 />
+      <Sidebar_78 />
+      <Hero_78 />
+      <Submenu_78 />
+    </>
+  )
+}
+
+export default App_78
